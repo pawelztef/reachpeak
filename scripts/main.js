29697bc9f6d2d3d92f74a4c010b60484fc1a80e5
pawelztef <pawelztef@gmail.com>
2018-09-29 12:20:58 +0100

    navigation affix

--- scripts/main.js
+++ scripts/main.js
@@ -1,3 +1,13 @@
 $(document).ready(function(){
+
   $('[data-toggle="tooltip"]').tooltip();
+
+  $(window).on('scroll', function(){ 
+    let screenRes = $(window).width();
+    if(screenRes > 832 && $(window).scrollTop() >= 35 ) {
+      $('.navbar-default').addClass('navbar-fixed');
+    } else {
+      $('.navbar-default').removeClass('navbar-fixed');
+    }
+  });
 });
